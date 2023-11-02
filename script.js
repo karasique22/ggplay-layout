563c58946132bb8cd3e62b7959f87d86c05a9df8
carasique22 <timofeidudin@mail.ru>
2023-11-02 17:34:40 +0300

    quickfix

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var startButton = document.getElementById("mainButton");
+var mainButton = document.getElementById("mainButton");
 
 mainButton.addEventListener("click", function () {
   window.open("start.html", "_blank");
